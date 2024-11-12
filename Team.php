347d745be7fa7ea30ed91155cb3ec1f385d07cbc
unknown <estherakinlade04@gmail.com>
2024-11-12 14:22:06 +0000

    part 1 working

---
+++ Team.php
@@ -0,0 +1,51 @@
+<?php
+
+class Team {
+    private $teamName;
+    private $totalPoints = 0;
+    private $totalGoals = 0;
+    private $totalGames = 0;
+
+    // Constructor
+    public function __construct($teamName) {
+        $this->teamName = $teamName;
+    }
+
+    // Method to calculate and update score
+    public function finalScore($homeScore, $opposingScore) {
+        // Update goals scored in this game to totalGoals
+        $this->totalGoals += $homeScore;
+        
+        // Increase totalGames count by 1
+        $this->totalGames += 1;
+
+        // Determine game result and update points accordingly
+        if ($homeScore > $opposingScore) {
+            // Home team won
+            $this->totalPoints += 3;
+        } elseif ($homeScore == $opposingScore) {
+            // Draw
+            $this->totalPoints += 1;
+        }
+        // If homeScore < opposingScore, no points added for a loss
+    }
+
+    // Getters to access private properties
+    public function getTeamName() {
+        return $this->teamName;
+    }
+
+    public function getTotalPoints() {
+        return $this->totalPoints;
+    }
+
+    public function getTotalGoals() {
+        return $this->totalGoals;
+    }
+
+    public function getTotalGames() {
+        return $this->totalGames;
+    }
+}
+
+?>
--- teamTester.php
+++ teamTester.php
@@ -1,12 +1,23 @@
 <?php
+// Enable error reporting to help with debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-include("Team.php");
+// Include the Team class file
+require_once("Team.php");
 
+// Create a new team instance
 $myTeam = new Team("Tigers");
 
-$myTeam->finalScore(2,2);
+// Test the finalScore method with a 2-2 draw
+$myTeam->finalScore(2, 2);
 
-echo "That worked";
+echo "That worked\n";
 
+// Output the internal state of the team object to verify correctness
+echo "Team Name: " . $myTeam->getTeamName() . "\n";
+echo "Total Points: " . $myTeam->getTotalPoints() . "\n"; // Should be 1 point for a draw
+echo "Total Goals: " . $myTeam->getTotalGoals() . "\n"; // Should be 2 goals scored
+echo "Total Games: " . $myTeam->getTotalGames() . "\n"; // Should be 1 game played
 
 ?>
